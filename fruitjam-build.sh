0753cef012ea37a93bfd06a4d624319c7a86a164
ladyada <limor@ladyada.net>
2025-03-19 09:39:46 -0500

    Add ability to specify disk image to fruitjam-build

--- fruitjam-build.sh
+++ fruitjam-build.sh
@@ -5,13 +5,15 @@ set -e
 # ./fruitjam-build.sh  -v         # vga resolution, no psram, 128KiB
 # ./fruitjam-build.sh  -v -m448   # vga resolution, no psram,  448KiB
 # ./fruitjam-build.sh  -m4096     # 512x342 resolution, psram, 4096KiB
+# ./fruitjam-build.sh  -d disk.img  # specify disk image
 
 DISP_WIDTH=512
 DISP_HEIGHT=342
-MEMSIZE=128
+MEMSIZE=400
+DISK_IMAGE=""
 CMAKE_ARGS=
 
-while getopts "hvm:" o; do
+while getopts "hvd:m:" o; do
     case "$o" in
     (v)
         DISP_WIDTH=640
@@ -21,11 +23,15 @@ while getopts "hvm:" o; do
     (m)
         MEMSIZE=$OPTARG
         ;;
+    (d)
+        DISK_IMAGE=$OPTARG
+        ;;
     (h|?)
-        echo "Usage: $0 [-v] [-m KiB]"
+        echo "Usage: $0 [-v] [-m KiB] [-d diskimage]"
         echo ""
         echo "   -v: Use framebuffer resolution 640x480 instead of 512x342"
         echo "   -m: Set memory size in KiB"
+        echo "   -d: Specify disk image to include"
         echo ""
         echo "PSRAM is automatically set depending on memory & framebuffer details"
         exit
@@ -34,10 +40,17 @@ while getopts "hvm:" o; do
 done
 
 TAG=fruitjam_${DISP_WIDTH}x${DISP_HEIGHT}_${MEMSIZE}k
-PSRAM=$((MEMSIZE > 448 || DISP_WIDTH != 640))
+PSRAM=$((MEMSIZE > 448 || DISP_WIDTH > 640))
 if [ $PSRAM -ne 0 ] ; then
     TAG=${TAG}_psram
     CMAKE_ARGS="$CMAKE_ARGS -DUSE_PSRAM=1"
+fi
+
+# Append disk name to build directory if disk image is specified
+if [ -n "$DISK_IMAGE" ] && [ -f "$DISK_IMAGE" ]; then
+    # Extract filename without extension
+    DISK_NAME=$(basename "$DISK_IMAGE" | sed 's/\.[^.]*$//')
+    TAG=${TAG}_${DISK_NAME}
 fi
 
 set -x
@@ -47,10 +60,13 @@ rm -f rom.bin
 ./external/umac/main -r '4D1F8172 - MacPlus v3.ROM' -W rom.bin || true
 [ -f rom.bin ]
 xxd -i < rom.bin > incbin/umac-rom.h
+if [ -n "$DISK_IMAGE" ] && [ -f "$DISK_IMAGE" ]; then
+    xxd -i < "$DISK_IMAGE" > incbin/umac-disc.h
+fi
 rm -rf build_${TAG}
 cmake -S . -B build_${TAG} \
     -DPICO_SDK_PATH=../pico-sdk \
-    -DBOARD=adafruit_fruit_jam -DPICO_BOARD=pico2 \
+    -DBOARD=adafruit_fruit_jam -DPICO_BOARD=pimoroni_pico_plus2_rp2350 \
     -DMEMSIZE=${MEMSIZE} \
     -DUSE_HSTX=1 \
     -DSD_TX=35 -DSD_RX=36 -DSD_SCK=34 -DSD_CS=39 -DUSE_SD=1 \
